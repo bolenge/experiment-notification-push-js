811e43e52c33f26b4d36f86abe6888f2845786c7
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-11-15 10:56:15 +0100

    feat add load assets files

--- views/index.php
+++ views/index.php
@@ -1,4 +1,6 @@
 <div>
   <h1>Bienvenue ici...</h1>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi id quo ad praesentium, atque quisquam autem nesciunt mollitia, est culpa architecto ex, accusantium dolores voluptatum alias incidunt saepe laborum. Debitis?</p>
+
+  <div id="push-permission"></div>
 </div>
--- views/layout.php
+++ views/layout.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?></title>
         <link rel="stylesheet" href="/public/css/style.css">
+        <script src="/public/js/main.js" defer></script>
     </head>
     <body>
         <?= $content ?>
